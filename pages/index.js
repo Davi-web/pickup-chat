07ffe6928df4ca75d04955cec9ab7544f57a7e48
Davi-web <jeseung.ha@vanderbilt.edu>
2022-12-05 23:16:18 -0600

    a

--- pages/index.js
+++ pages/index.js
@@ -50,27 +50,6 @@ const Auth = () => {
       <div className="auth-container">
         <form className="auth-form" onSubmit={(e) => onSubmit(e)}>
           <div className="auth-title">PickUp Chat</div>
-
-          <div className="input-container">
-            <input
-              placeholder="Email"
-              className="text-input"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-
-          <div className="input-container">
-            <input
-              type="password"
-              placeholder="Password"
-              className="text-input"
-              onChange={(e) => setSecret(e.target.value)}
-            />
-          </div>
-
-          <button type="submit" className="submit-button">
-            Login / Sign Up
-          </button>
         </form>
       </div>
     </div>
