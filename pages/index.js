495b882ffa848ac7990033fb916c684a7a809aab
Davi-web <jeseung.ha@vanderbilt.edu>
2022-12-05 21:13:24 -0600

    Aded changes

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ const MessageFormSocial = dynamic(() =>
 const Home = () => {
   const [showChat, setShowChat] = useState(false);
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const { username, secret } = router.query;
   console.log(username, secret);
 
@@ -19,8 +20,8 @@ const Home = () => {
     setTimeout(() => {
       if (typeof document !== undefined) {
         setShowChat(true);
+        setLoading(false);
       }
-      setLoading(false);
     }, 2000);
   }, []);
 
@@ -29,13 +30,19 @@ const Home = () => {
   return (
     <div className="background">
       <div className="shadow">
-        <ChatEngine
-          height="calc(100vh - 212px)"
-          projectID="98554d93-5bcc-4c28-8594-ba95b6672043"
-          userName={username}
-          userSecret={secret}
-          renderNewMessageForm={() => <MessageFormSocial />}
-        />
+        {loading ? (
+          <div className="loader-container">
+            <div className="spinner"></div>
+          </div>
+        ) : (
+          <ChatEngine
+            height="calc(100vh - 212px)"
+            projectID="98554d93-5bcc-4c28-8594-ba95b6672043"
+            userName={username}
+            userSecret={secret}
+            renderNewMessageForm={() => <MessageFormSocial />}
+          />
+        )}
       </div>
     </div>
   );
