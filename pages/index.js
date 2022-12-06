3168ab1efee8bbd381aff07e9e432d42ea8e3fa4
Davi-web <jeseung.ha@vanderbilt.edu>
2022-12-06 00:38:30 -0600

    a

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../context";
 import { useRouter } from "next/router";
 import axios from "axios";
